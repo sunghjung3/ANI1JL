21a8ada2cb37e7ada8da63a8be6997cb1d74a0f5
Sung Jung <sunghjung3@utexas.edu>
2022-03-20 15:23:01 -0500

    created training.jl file in test directory

--- test/training.jl
+++ test/training.jl
@@ -1,8 +1,21 @@
 using PyCall
 
-py"""
-import numpy
-xs = numpy.ones((2, 3))
-"""
+data_file_name = "C:\\Users\\sungh\\code\\ANI-1_release\\ani_gdb_s02.h5"
 
-@info xs
+#==============================================================================================#
+function import_ani_data(filename::String)
+    include("../tools/pyanitools.jl")
+    adl = py"anidataloader"(filename)
+
+    for data in adl
+        @info typeof(data)
+    end
+end
+
+#==============================================================================================#
+function main(data_file_name::String)
+    import_ani_data(data_file_name)
+end
+
+
+main(data_file_name)
