60f5074be33d32d0c4e8e95bde348010f910ead0
Sung Jung <sunghjung3@utexas.edu>
2022-03-22 07:26:52 -0500

    added BPParameters struct to organize all the parameters

--- src/fingerprints.jl
+++ src/fingerprints.jl
@@ -1,11 +1,20 @@
 """
-Methods to compute fingerprints
+Methods to compute Behler-Parrinello Descriptors
 """
 
 module fingerprints
 
 
 floats_or_ints = Union{Float64, Int64}
+
+
+struct BPParameters
+    R_c::floats_or_ints  # cutoff radius, as used in the cutoff function
+    R_s::floats_or_ints  # shifting distance, as used in the radial element of AEV
+    η::floats_or_ints  # width parameter in the exponential term
+    θ_s::floats_or_ints  # shifting angle, as used in the angular element of AEV
+    ζ::floats_or_ints  # angular element width parameter
+end
 
 
 function f_C(R_ij::floats_or_ints, R_c::floats_or_ints) :: Float64
@@ -22,28 +31,27 @@ function f_C(R_ij::floats_or_ints, R_c::floats_or_ints) :: Float64
 end
 
 
-function G_R_singleTerm(R_ij::floats_or_ints, R_s::floats_or_ints, R_c::floats_or_ints,
-                        η::floats_or_ints) :: Float64
+function G_R_singleTerm(R_ij::floats_or_ints, p::BPParameters) :: Float64
     """
     Single term of the radial element G_R of the atomic environment vector
 
         G_R_i = exp(-η (R_ij - R_s)^2 ) * f_C(R_ij)
     """
-    diff = R_ij - R_s
-    return exp(-η * diff * diff) * f_C(R_ij, R_c)
+    diff = R_ij - p.R_s
+    return exp(-p.η * diff * diff) * f_C(R_ij, p.R_c)
 end
 
 
-function G_A_singleTerm(θ_ijk::floats_or_ints, θ_s::floats_or_ints, ζ::floats_or_ints,
-                        R_ij::floats_or_ints, R_ik::floats_or_ints, R_s::floats_or_ints,
-                        R_c::floats_or_ints, η::floats_or_ints) :: Float64
+function G_A_singleTerm(θ_ijk::floats_or_ints, R_ij::floats_or_ints, R_ik::floats_or_ints,
+                        p::BPParameters) :: Float64
     """
     Single term of the angular element G_A of the atomic environment vector
 
         G_A_i = (1 + cos(θ_ijk- θ_s))^ζ * exp(-η([R_ij + R_ik]/2 - R_s)^2) * f_C(R_ij) f_C(R_ik)
     """
-    diff = (R_ij + R_ik) / 2 - R_s
-    return (1 + cos(θ_ijk - θ_s))^ζ * exp(-η * diff * diff) * f_C(R_ij, R_c) * f_C(R_ik, R_c)
+    diff = (R_ij + R_ik) / 2 - p.R_s
+    f = f_C(R_ij, p.R_c) * f_C(R_ik, p.R_c)
+    return (1 + cos(θ_ijk - p.θ_s))^p.ζ * exp(-p.η * diff * diff) * f
 end
 
 
