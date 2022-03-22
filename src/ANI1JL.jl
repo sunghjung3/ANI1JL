e0a4b289fa3e2a980ff5aa479daa70713c0eceb4
Sung Jung <sunghjung3@utexas.edu>
2022-03-22 07:57:33 -0500

    created training directory within src

--- src/ANI1JL.jl
+++ src/ANI1JL.jl
@@ -1,14 +1,33 @@
 # main file that brings everything together
-include("fingerprints.jl")
+include("training/fingerprints.jl")
 
 module ANI1JL
 
 
-function train(symbols::Vector{Vector{String}},
-               coordinates::Vector{Matrix{Float64}},
-               energies::Vector{Float64})
+function train(symbols::Vector{Vector{String}},        # required
+               coordinates::Vector{Matrix{Float64}},   # required
+               energies::Vector{Float64};              # required
+               settingsFile=nothing                    # optional (keyword)
+               )
     """
     Trains the force field with given data.                                                    ! optional parameters specifying what parameters to use (make it have default value)
+
+    Required training data (let's say there are N datapoints):
+    - symbols:
+        vector of length N, each element being a vector of chemical symbols
+    - coordinates:
+        vector of length N, each element being a matrix of atomic coordinates (same order as
+        the corresponding element in the "symbols" vector)
+            * Each atom's (x, y, z) as a COLUMN in the matrix
+    - energies:
+        vector of length N, each element being the true energy of the corresponding system in
+        the "coordinates" vector
+
+    Other required arguments:
+    (None at the moment)
+
+    Optional arguments:
+    - 
 
     Workflow:
         * Take symbols, coordinates, and energies as input
