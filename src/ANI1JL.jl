5bcf39496944bcd0cbc47a3c432cf54608253490
Sung Jung <sunghjung3@utexas.edu>
2022-04-13 16:19:49 -0500

    clean up

--- src/ANI1JL.jl
+++ src/ANI1JL.jl
@@ -18,47 +18,9 @@ include("../tools/fileio.jl")
 export fileio  # the module
 
 include("../tools/pyanitools.jl")
-export pyanitools
+export pyanitools  # the module
 
-
-
-"""
-Required training data (let's say there are N datapoints):
-- symbols:
-    vector of length N, each element being a vector of chemical symbols
-- coordinates:
-    vector of length N, each element being a matrix of atomic coordinates (same order as
-    the corresponding element in the "symbols" vector)
-        * Each atom's (x, y, z) as a COLUMN in the matrix
-- energies:
-    vector of length N, each element being the true energy of the corresponding system in
-    the "coordinates" vector
-
-Other required arguments:
-(None at the moment)
-
-Optional arguments:
-- parameterFile:
-    File containing settings and parameters to use for training
-        * uses default settings if not provided 
-
-Workflow:
-    * Take symbols, coordinates, and energies as input
-    * should change all datatypes to 32 bit
-    * Calculate distance and angle matrices from coordinates and symbols
-    * Compute atomic environment vectors (AEV) from distance and angle matrices
-        * to vectorize training, create unnecessary AEVs too ?
-    * Pre-process data (mean = 0, std = 1)
-    * initalizing weights: "He initialization for RELU"
-        * https://stats.stackexchange.com/questions/229885/whats-the-recommended-weight-initialization-strategy-when-using-the-elu-activat
-        * but the ANI paper initialized from random normal distribution between the ranges (-1/d, 1/d), where d is the # of inputs into the node
-    * pre-process results in each layer? (batch normalization)
-    * Train and return the model (multiple times, each with smaller learning rate)
-        * max norm regularization
-        * first training: vectorized (0 inputs); subsequent trainings with no 0 inputs ?
-"""
-
-#=
+#= Old
 function train(symbols::Vector{Vector{String}},              # required
                coordinates::Vector{Matrix{Float32}},         # required
                energies::Vector{Float32};                    # required
--- test/training.jl
+++ test/training.jl
@@ -5,6 +5,9 @@ using ANI1JL
 using PyCall
 
 #==============================================================================================#
+# Helper functions for this script
+
+
 function import_ani_data(filename::String)
     adl = pyanitools.anidataloader(filename)
 
@@ -34,15 +37,60 @@ function import_ani_data(filename::String)
     return symbols, coordinates, energies
 end
 
+
 #==============================================================================================#
-function main()
-    data_file_name = raw"C:\Users\sungh\code\ANI1JL\test\data\ani_gdb_s01.h5"
-    symbols, coordinates, energies = import_ani_data(data_file_name)
-    # model = ANI1JL.train(symbols, coordinates, energies)
+#=================================== Start of training script =================================#
+
+#=
+Required training data (let's say there are N datapoints):
+- symbols:
+    vector of length N, each element being a vector of chemical symbols
+- coordinates:
+    vector of length N, each element being a matrix of atomic coordinates (same order as
+    the corresponding element in the "symbols" vector)
+        * Each atom's (x, y, z) as a COLUMN in the matrix
+- energies:
+    vector of length N, each element being the true energy of the corresponding system in
+    the "coordinates" vector
+
+Workflow:
+    * Take symbols, coordinates, and energies as input
+    * should change all datatypes to 32 bit
+    * Calculate distance and angle matrices from coordinates and symbols
+    * Compute atomic environment vectors (AEV) from distance and angle matrices
+        * to vectorize training, create unnecessary AEVs too ?
+    * Pre-process data (mean = 0, std = 1)
+    * initalizing weights: "He initialization for RELU"
+        * https://stats.stackexchange.com/questions/229885/whats-the-recommended-weight-initialization-strategy-when-using-the-elu-activat
+        * but the ANI paper initialized from random normal distribution between the ranges (-1/d, 1/d), where d is the # of inputs into the node
+    * pre-process results in each layer? (batch normalization)
+    * Train and return the model (multiple times, each with smaller learning rate)
+        * max norm regularization
+        * first training: vectorized (0 inputs); subsequent trainings with no 0 inputs ?
+=#
 
 
-    # println(model)
-end
+
+#=
+# load data
+data_file_name = raw"C:\Users\sungh\code\ANI1JL\test\data\ani_gdb_s01.h5"
+symbols, coordinates, energies = import_ani_data(data_file_name)
+
+# set parameters for AEVs and NN (using default here, but can also specify path to a .par file)
+params = set_params()
+println(params)
+
+# make AEVs for all data points and save to file
+AEVs_list = coordinates_to_AEVs(params, symbols, coordinates)
+save_AEVs("ani_gdb_s01_AEVs.bson", params, AEVs_list)
+=#
+
+# load existing AEV files
+AEVs_filenames = [raw"C:\Users\sungh\code\ANI1JL\test\AEVs\ani_gdb_s01_AEVs.bson",
+                  raw"C:\Users\sungh\code\ANI1JL\test\AEVs\ani_gdb_s02_AEVs.bson"
+                ]
+    # @show length(AEVs)]
+params, AEVs = load_AEVs(AEVs_filenames...);
 
 
-main()
+println("Training script finished")
